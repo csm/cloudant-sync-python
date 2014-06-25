25293d45b2b264e6b17ea195a5140b226da60fe1
Casey Marshall <casey.s.marshall@gmail.com>
2014-06-25 14:29:38 -0700

    Make datastore db handle thread local. Complete some force insert stuff (still to be tested). Use __invoke_callbacks helper.

--- cloudant/sync/datastore/Datastore.py
+++ cloudant/sync/datastore/Datastore.py
@@ -21,6 +21,7 @@ import itertools
 import os
 import sqlite3
 import tempfile
+import threading
 
 from .Attachment import UnsavedBytesAttachment
 from .AttachmentManager import AttachmentManager, PreparedAttachment
@@ -47,30 +48,39 @@ class Datastore(object):
             self.__path = path
             self.__name = name
             self.__extensions_dir = os.path.join(path, "extensions")
-            dbfile = os.path.join(path, self.DB_FILE_NAME)
-            self.__db = Database(sqlite3.connect(dbfile), on_stmt)
+            self.__dbfile = os.path.join(path, self.DB_FILE_NAME)
         else:
             self.__path = ':memory:'
             self.__name = name
             self.__extensions_dir = None
-            self.__db = Database(sqlite3.connect(':memory:'), on_stmt)
+            self.__dbfile = ':memory:'
+        self.__onstmt = on_stmt
+        self.__dblocal = threading.local()
         self.__update_schema(SCHEMA_VERSION_3, 3)
         self.__update_schema(getSCHEMA_VERSION_4(), 4)
         self.__callbacks = {}
         self.__attachment_manager = AttachmentManager(self)
 
+    def __get_connection(self):
+        try:
+            return self.__dblocal.db
+        except AttributeError:
+            self.__dblocal.db = Database(sqlite3.connect(self.__dbfile), self.__onstmt)
+            return self.__dblocal.db
+
     def __update_schema(self, schema, version):
-        self.__db.execute('PRAGMA foreign_keys = ON;')
-        dbversion = self.__db.get_version()
+        db = self.__get_connection()
+        db.execute('PRAGMA foreign_keys = ON;')
+        dbversion = db.get_version()
         if dbversion < version:
             try:
-                self.__db.begin_transaction()
+                db.begin_transaction()
                 for statement in schema:
-                    self.__db.execute(statement)
-                self.__db.execute('PRAGMA user_version = %s;' % version)
-                self.__db.set_transaction_success()
+                    db.execute(statement)
+                db.execute('PRAGMA user_version = %s;' % version)
+                db.set_transaction_success()
             finally:
-                self.__db.end_transaction()
+                db.end_transaction()
 
     def extension_data_dir(self, extension):
         if self.__extensions_dir is not None:
@@ -94,16 +104,16 @@ class Datastore(object):
             pass
 
     def get_last_sequence(self):
-        return self.__db.execute('SELECT max(sequence) FROM revs;').fetchone()[0]
+        return self.__get_connection().execute('SELECT max(sequence) FROM revs;').fetchone()[0]
 
     def __len__(self):
-        return self.__db.execute('SELECT count(DISTINCT doc_id) FROM revs WHERE current=1 AND deleted=0').fetchone()[0]
+        return self.__get_connection().execute('SELECT count(DISTINCT doc_id) FROM revs WHERE current=1 AND deleted=0').fetchone()[0]
 
     def __contains__(self, item):
         return self.get(item) is not None
 
     def get_sql(self):
-        return self.__db
+        return self.__get_connection()
 
     def get(self, doc_id, rev=None):
         if rev is None:
@@ -114,11 +124,11 @@ class Datastore(object):
             args = (doc_id, rev)
             sql = 'SELECT ' + self.FULL_DOC_IDS + ' FROM revs, docs' \
                   ' WHERE docs.docid=? AND revs.doc_id=docs.doc_id AND revid=? LIMIT 1'
-        c = self.__db.execute(sql, args)
+        c = self.__get_connection().execute(sql, args)
         return DocumentRevision.from_cursor(c)
 
     def get_local(self, doc_id, rev=None):
-        cursor = self.__db.execute('SELECT revid, json FROM localdocs WHERE docid=?', [doc_id])
+        cursor = self.__get_connection().execute('SELECT revid, json FROM localdocs WHERE docid=?', [doc_id])
         res = cursor.fetchone()
         if res is not None:
             rev_id = res[0]
@@ -130,7 +140,7 @@ class Datastore(object):
         return None
 
     def get_numeric_id(self, docid):
-        c = self.__db.execute('SELECT doc_id FROM docs WHERE docid = ?', (docid,))
+        c = self.__get_connection().execute('SELECT doc_id FROM docs WHERE docid = ?', (docid,))
         result = c.fetchone()
         if result is not None and result[0] is not None:
             return long(result[0])
@@ -144,7 +154,7 @@ class Datastore(object):
         sql = 'SELECT ' + self.FULL_DOC_IDS + ' FROM revs, docs' \
               ' WHERE revs.doc_id=? AND revs.doc_id = docs.doc_id ORDER BY sequence ASC'
         tree = DocumentRevisionTree()
-        c = self.__db.execute(sql, (docid,))
+        c = self.__get_connection().execute(sql, (docid,))
         while True:
             rev = DocumentRevision.from_cursor(c)
             if rev is None:
@@ -154,7 +164,7 @@ class Datastore(object):
 
     def changes(self, since, limit):
         since = min(0, since)
-        c = self.__db.execute('SELECT doc_id, max(sequence) FROM revs'
+        c = self.__get_connection().execute('SELECT doc_id, max(sequence) FROM revs'
                               ' WHERE sequence > ? AND sequence <= ? GROUP BY doc_id', (since, since + limit))
         ids = map(lambda r: r[0], c.fetchall())
         last_seq = max(ids)
@@ -171,7 +181,7 @@ class Datastore(object):
             sql = 'SELECT ' + self.FULL_DOC_IDS + ' FROM revs, docs' \
                   ' WHERE revs.doc_id IN ( %s ) AND current = 1 AND docs.doc_id = revs.doc_id' % \
                   ','.join(['?' for _ in xrange(0, len(part))])
-            c = self.__db.execute(sql, part)
+            c = self.__get_connection().execute(sql, part)
             result += map(DocumentRevision.from_row, c)
         result.sort(key=lambda d: d.sequence)
         return result
@@ -184,14 +194,14 @@ class Datastore(object):
         sql = 'SELECT ' + self.FULL_DOC_IDS + ' FROM revs, docs' \
               ' WHERE deleted = 0 AND current = 1 AND docs.doc_id = revs.doc_id' \
               ' ORDER BY docs.doc_id %s, revid DESC LIMIT %d OFFSET %d' % (order, limit, offset)
-        c = self.__db.execute(sql)
+        c = self.__get_connection().execute(sql)
         return map(DocumentRevision.from_row, c)
 
     def get_by_ids(self, doc_ids):
         sql = 'SELECT ' + self.FULL_DOC_IDS + ' FROM revs, docs' \
               ' WHERE docid ( %s ) AND current = 1 AND docs.doc_id = revs.doc_id' \
               ' ORDER BY docs.doc_id' % ','.join('?' for _ in xrange(0, len(doc_ids)))
-        c = self.__db.execute(sql, doc_ids)
+        c = self.__get_connection().execute(sql, doc_ids)
         return map(DocumentRevision.from_row, c)
 
     def create(self, body, doc_id=None):
@@ -201,7 +211,8 @@ class Datastore(object):
             doc_id = str(uuid.uuid4()).replace('-', '')
         if any(map(lambda key: key.startswith('_'), body.to_dict().keys())):
             raise ValueError('documents may not have attributes that begin with "_"')
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         event = None
         try:
             numeric_id = self.__insert_doc_id(doc_id)
@@ -211,37 +222,32 @@ class Datastore(object):
             new_seq = self.__insert_rev(numeric_id, rev_id, -1, False, True, body.to_bytes(), True)
             doc = self.get(doc_id, rev_id)
             event = DocumentCreated(doc)
-            self.__db.set_transaction_success()
+            db.set_transaction_success()
             return doc
         except sqlite3.IntegrityError:
             raise ConflictError('document with that ID already exists')
         finally:
-            if event is not None:
-                l = self.__callbacks.get('DocumentCreated', [])
-                for cb in l:
-                    try:
-                        cb(event)
-                    except:
-                        pass
-            self.__db.end_transaction()
+            self.__invoke_callbacks('DocumentCreated', event)
+            db.end_transaction()
 
     def create_local(self, body, doc_id=None):
         if not isinstance(body, DocumentBody):
             raise ValueError('expected a DocumentBody')
         if doc_id is None:
             doc_id = str(uuid.uuid4()).replace('-', '')
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         try:
             values = {'docid': doc_id,
                       'revid': '1-local',
                       'json': body.to_bytes()}
-            num_id = self.__db.insert('localdocs', values)
+            num_id = db.insert('localdocs', values)
             if num_id < 0:
                 raise ConflictError('Can not insert new local doc, likely the docId exists already: ' + doc_id)
-            self.__db.set_transaction_success()
+            db.set_transaction_success()
             return self.get_local(doc_id)
         finally:
-            self.__db.end_transaction()
+            db.end_transaction()
 
     def update(self, doc_id, prev_revid, body):
         if not isinstance(body, DocumentBody):
@@ -254,7 +260,8 @@ class Datastore(object):
             raise ValueError('documents may not have attributes that begin with "_"')
         int(prev_revid.split('-')[0])
         updated = None
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         try:
             prev_rev = self.get(doc_id, prev_revid)
             if prev_rev is None:
@@ -264,18 +271,12 @@ class Datastore(object):
             self.__checkoff_prev_winner(prev_rev)
             new_revid = self.__insert_new_winner(body, prev_rev)
             new_rev = self.get(doc_id, new_revid)
-            self.__db.set_transaction_success()
+            db.set_transaction_success()
             updated = DocumentUpdated(prev_rev, new_rev)
             return new_rev
         finally:
-            if updated is not None:
-                l = self.__callbacks.get('DocumentUpdated', [])
-                for cb in l:
-                    try:
-                        cb(updated)
-                    except:
-                        pass
-            self.__db.end_transaction()
+            self.__invoke_callbacks('DocumentUpdated', updated)
+            db.end_transaction()
 
     def update_local(self, doc_id, prev_revid, body):
         if not isinstance(body, DocumentBody):
@@ -286,30 +287,32 @@ class Datastore(object):
             raise ValueError('must have a prev_rev to update')
         prev_generation = int(prev_revid.split('-')[0])
         pre_rev = self.get_local(doc_id, prev_revid)
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         try:
             new_revid = '%d-local' % (prev_generation + 1)
             values = {'revid': new_revid,
                       'json': body.to_bytes()}
             where_args = (doc_id, prev_revid)
-            updated = self.__db.update('localdocs', values, 'docid=? AND revid=?', where_args)
+            updated = db.update('localdocs', values, 'docid=? AND revid=?', where_args)
             if updated == 1:
-                self.__db.set_transaction_success()
+                db.set_transaction_success()
                 return self.get_local(doc_id, new_revid)
             else:
                 raise ConflictError('error updating local docs: ' + pre_rev)
         finally:
-            self.__db.end_transaction()
+            db.end_transaction()
 
     def delete(self, doc_id, prev_revid):
-        if self.__db.is_closed():
+        db = self.__get_connection()
+        if db.is_closed():
             raise ValueError('database is closed')
         if doc_id is None or len(doc_id) == 0:
             raise ValueError('must have a doc_id to delete')
         if prev_revid is None or len(prev_revid) == 0:
             raise ValueError('must have a prev_revid to delete')
         doc_deleted = None
-        self.__db.begin_transaction()
+        db.begin_transaction()
         try:
             pre_rev = self.get(doc_id, prev_revid)
             if pre_rev is None:
@@ -325,26 +328,20 @@ class Datastore(object):
                 self.__insert_rev(pre_rev.internal_id, new_revid, pre_rev.sequence, True, pre_rev.current, '{}', False)
                 new_rev = self.get(doc_id, new_revid)
                 doc_deleted = DocumentDeleted(pre_rev, new_rev)
-            self.__db.set_transaction_success()
+            db.set_transaction_success()
         finally:
-            if doc_deleted is not None:
-                l = self.__callbacks.get('DocumentDeleted', [])
-                for cb in l:
-                    try:
-                        cb(doc_deleted)
-                    except:
-                        pass
-            self.__db.end_transaction()
+            self.__invoke_callbacks('DocumentDeleted', doc_deleted)
+            db.end_transaction()
 
     def delete_local(self, doc_id):
         if doc_id is None or len(doc_id) == 0:
             raise ValueError('document ID cannot be empty')
-        rows_del = self.__db.delete('localdocs', 'docid=?', (doc_id,))
+        rows_del = self.__get_connection().delete('localdocs', 'docid=?', (doc_id,))
         if rows_del == 0:
             raise DocumentNotFoundError('no local doc with id: ' + doc_id)
 
     def get_public_id(self):
-        c = self.__db.execute('SELECT value FROM info WHERE key=\'publicUUID\'')
+        c = self.__get_connection().execute('SELECT value FROM info WHERE key=\'publicUUID\'')
         row = c.fetchone()
         if row is None:
             raise ValueError('failed to query publicUUID; is the DB initialized?')
@@ -363,7 +360,8 @@ class Datastore(object):
         doc_created = None
         doc_updated = None
         ok = True
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         try:
             if doc_rev.docid in self:
                 seq = self.__do_force_insert(doc_rev, rev_history)
@@ -388,11 +386,20 @@ class Datastore(object):
                         ok = False
                         break
             if ok:
-                self.__db.set_transaction_success()
+                db.set_transaction_success()
         finally:
             self.__invoke_callbacks('DocumentCreated', doc_created)
             self.__invoke_callbacks('DocumentUpdated', doc_updated)
-            self.__db.end_transaction()
+            db.end_transaction()
+
+    def __do_force_create(self, doc_rev, rev_history):
+        assert not doc_rev.docid in self
+        numeric_id = self.__insert_doc_id(doc_rev.docid)
+        parent_seq = 0L
+        for rev in rev_history[:-1]:
+            parent_seq = self.__insert_stub_rev(numeric_id, rev, parent_seq)
+        return self.__insert_rev(numeric_id, rev_history[-1], parent_seq, doc_rev.deleted, True,
+                                 doc_rev.body.to_bytes(), True)
 
     def __do_force_insert(self, doc_rev, rev_history):
         numeric_id = self.get_numeric_id(doc_rev.docid)
@@ -408,7 +415,7 @@ class Datastore(object):
     def __insert_into_new_tree(self, doc_rev, rev_history, numeric_id, local_revs):
         prev_winner = local_revs.current_rev()
         parent_seq = 0L
-        for rev in rev_history:
+        for rev in rev_history[:-1]:
             parent_seq = self.__insert_stub_rev(numeric_id, rev, parent_seq)
             new_node = self.get(doc_rev.docid, rev)
             local_revs.add(new_node)
@@ -473,7 +480,7 @@ class Datastore(object):
                 self.__change_to_not_current(previous_leaf.sequence)
 
     def __change_to_not_current(self, seq):
-        self.__db.update('revs', {'current': 0}, 'sequence=?', (seq,))
+        self.__get_connection().update('revs', {'current': 0}, 'sequence=?', (seq,))
 
     def __invoke_callbacks(self, tag, value):
         if value is not None:
@@ -501,13 +508,14 @@ class Datastore(object):
 
     def __checkoff_prev_winner(self, winner):
         values = {'current': 0}
-        self.__db.update('revs', values, 'sequence=?', (winner.sequence,))
+        self.__get_connection().update('revs', values, 'sequence=?', (winner.sequence,))
 
     def __insert_doc_id(self, docid):
-        return self.__db.insert('docs', {'docid': docid})
+        return self.__get_connection().insert('docs', {'docid': docid})
 
     def __insert_rev(self, numeric_id, rev_id, parent_seq, deleted, current, data, available):
-        self.__db.begin_transaction()
+        db = self.__get_connection()
+        db.begin_transaction()
         try:
             args = {'doc_id': numeric_id,
                     'revid': rev_id,
@@ -517,17 +525,17 @@ class Datastore(object):
                     'json': data}
             if parent_seq > 0:
                 args['parent'] = parent_seq
-            new_seq = self.__db.insert('revs', args)
+            new_seq = db.insert('revs', args)
 
             att_sql = 'INSERT INTO attachments ' \
                       '(sequence, filename, key, type, length, revpos) ' \
                       'SELECT %d, filename, key, type, length, revpos ' \
                       'FROM attachments WHERE sequence=%d' % (new_seq, parent_seq)
-            self.__db.execute(att_sql)
-            self.__db.set_transaction_success()
+            db.execute(att_sql)
+            db.set_transaction_success()
             return new_seq
         finally:
-            self.__db.end_transaction()
+            db.end_transaction()
 
     name = property(lambda self: self.__name)
     last_sequence = property(get_last_sequence)
